8396c67559999f909e3f91d0953369dd94c87f4e
danielgebara <128635331+danielgebara@users.noreply.github.com>
2023-09-14 18:37:33 -0400

    Lab1

---
+++ Devoir0/D0_300401006/bonjour1.py
@@ -0,0 +1,6 @@
+# Comments
+# Student name: Daniel Gebara 
+# Students id: 300401006
+# This program program prints a message
+
+print("Bonjour", 'Daniel')
---
+++ Devoir0/D0_300401006/bonjour2.py
@@ -0,0 +1,7 @@
+# Comments
+# Student name: Daniel Gebara 
+# Students id: 300401006
+# This program program prints a message
+
+x = input("Quelle est votre nom?: ")
+print("Bonjour", x)
