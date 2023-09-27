3b4bcb7156d99321f6030f84a11abd4dd3f39e83
danielgebara <128635331+danielgebara@users.noreply.github.com>
2023-09-26 20:18:37 -0400

    Updates

--- Devoir1/d1.py
+++ Devoir1/d1.py
@@ -52,6 +52,3 @@ def anneeBis(an):
     result = result and (an%100 != 0) and not (an%400 == 0)
     return result
 # return ((an>1582 ) and an % 4 == 0 and an % 100 != 0 and not an % 400 == 0)
-
-# Programme principal
-
